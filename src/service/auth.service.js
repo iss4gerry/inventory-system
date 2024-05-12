f3065595ee9a363c7af30d8a18bb70edc64caf71
Gerry <150305559+iss4gerry@users.noreply.github.com>
2024-05-12 12:08:46 +0700

    Update auth.service.js

--- src/service/auth.service.js
+++ src/service/auth.service.js
@@ -2,7 +2,7 @@ const httpStatus = require('http-status');
 const bcrypt = require('bcryptjs');
 const userService = require('./user.service');
 const ApiError = require('../utils/apiError');
- 
+  
 const loginUserWithEmailAndPassword = async (email, password) => {
   const user = await userService.getUserByEmail(email);
   if (!user) {
